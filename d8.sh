701005360179f518ff489cdba6d81d0865be9bd3
dai3 <vmvz@qq.com>
2018-07-16 07:26:35 +0800

    Update d8.sh

--- d8.sh
+++ d8.sh
@@ -6,4 +6,4 @@ deb-src http://mirrors.aliyun.com/debian/ jessie main non-free contrib
 deb-src http://mirrors.aliyun.com/debian/ jessie-proposed-updates main non-free contrib
 END
 apt-get -q -y update
-apt-get -y install htop lrzsz vim
+apt-get -y install htop lrzsz vim wget screen curl python
