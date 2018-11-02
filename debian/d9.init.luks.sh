7ebaac268689ff7cc1bf852b22c76073bafe5fd6
dai <dai@dai3.com>
2018-11-02 11:43:12 +0800

    d9
    d9

--- debian/d9.init.luks.sh
+++ debian/d9.init.luks.sh
@@ -10,4 +10,9 @@ deb http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
 deb-src http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
 END
 apt-get -q -y update
-apt-get -y install net-tools htop lrzsz vim wget screen curl
+apt-get -y install net-tools htop lrzsz vim wget screen curl git
+
+apt-get -y install yubikey-luks yubikey-personalization
+cd /usr/lib && git clone https://github.com/cornelinux/yubikey-luks.git
+cd yubikey-luks; cp yubikey-luks-suspend.service /etc/systemd/system; systemctl enable yubikey-luks-suspend.service
+
