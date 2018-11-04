c7d5c32d6a0c9e4f7ae9769a2aeebe938dbcf140
dai <Zny8Wec565v6>
2018-11-04 15:33:03 +0800

    d
    d

---
+++ debian/d9.init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+rm -rf /etc/apt/sources.list*
+cat > /etc/apt/sources.list <<END
+deb http://mirrors.aliyun.com/debian/ stretch main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ stretch main non-free contrib
+deb http://mirrors.aliyun.com/debian-security stretch/updates main
+deb-src http://mirrors.aliyun.com/debian-security stretch/updates main
+deb http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
+deb http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+END
+apt-get -q -y update
+apt-get -y install net-tools htop lrzsz vim wget screen curl git build-essential
+
+ifconfig
